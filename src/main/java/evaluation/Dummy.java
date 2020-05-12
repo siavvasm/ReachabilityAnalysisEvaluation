a1c98d5252cb4b40001b67802adc3922e103dcbd
Miltiadis <siavvasm@iti.gr>
2020-05-12 11:11:09 +0300

    FileReader and BufferReader - Read data from file added

--- src/main/java/evaluation/Dummy.java
+++ src/main/java/evaluation/Dummy.java
@@ -1,5 +1,7 @@
 package evaluation;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 import sec.ond.time.with.you.MoreDummy;
@@ -14,6 +16,7 @@ public class Dummy {
 		// TAINTED!
 		String str = Integer.toString(scanner.nextInt()); /* TAINTED */
 		
+		
 		printStr(str); /* SINK */
 		
 		
@@ -25,7 +28,30 @@ public class Dummy {
 		
 		moreDummy.printStrMoreDummy(x); 
 		MoreDummy.printStrMoreDummy(y);
+		
+		/*
+		 * Read Content from files
+		 */
+		
+		// 1. Create the appropriate readers
+		FileReader fr = new FileReader("path/to/file");
+		BufferedReader br = new BufferedReader(fr);
+		
+		// 2. Read a String from the selected file
+		String fileString = br.readLine(); /* TAINTED */
+		
+		// 3. Feed the String to different methods
+		
+		// 3.1 Print the String to the console
+		System.out.println(fileString);
+		
+		// 3.2 Print the String using a local method
+		printStr(fileString);
+		
+		// 3.3 Print the String using a internal method that belongs to the same project
+		MoreDummy.printStrMoreDummy(fileString);
 	}
+	
 	private static void printStr(String str) {
 		System.out.println(str);
 	}
