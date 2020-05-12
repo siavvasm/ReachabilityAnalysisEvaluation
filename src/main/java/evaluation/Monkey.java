630308949bcce603eba6840eb761a3aa8e47c6b2
Miltiadis <siavvasm@iti.gr>
2020-05-12 17:44:58 +0300

    Monkey Object added

---
+++ src/main/java/evaluation/Monkey.java
@@ -0,0 +1,55 @@
+package evaluation;
+
+public class Monkey {
+	
+	int x;
+	int y;
+	float z;
+	String name;
+	
+	public Monkey() {
+	}
+	
+	public Monkey(int x, int y, float z, String name) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.name = name;
+	}
+
+	public int getX() {
+		return x;
+	}
+	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public float getZ() {
+		return z;
+	}
+	
+	public void setZ(float z) {
+		this.z = z;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
