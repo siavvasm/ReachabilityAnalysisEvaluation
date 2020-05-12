50cedb436865f10e179ff217b76b83c50d9649bf
Miltiadis <siavvasm@iti.gr>
2020-05-12 10:59:26 +0300

    Initial Commit - Dummy classes added

--- src/main/java/sec/ond/time/with/you/MoreDummy.java
+++ src/main/java/sec/ond/time/with/you/MoreDummy.java
@@ -6,8 +6,12 @@ import java.util.Scanner;
 public class MoreDummy {
 
 	private void executeThisMoreDummyCode() throws IOException {
+		
+		// 1. Retrieve data from the console
 		Scanner scanner = new Scanner(System.in);
-		String str1 = Integer.toString(scanner.nextInt());
+		String str1 = Integer.toString(scanner.nextInt()); /* TAINTED */
+		
+		// 2. Pass the tainted data to another method
 		printStrMoreDummy(str1);
 	}
 	public static void printStrMoreDummy(String str) {
